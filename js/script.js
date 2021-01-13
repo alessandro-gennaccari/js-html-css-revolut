817ceb07792bc87f697ee14c9ae4293473cb9fed
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-01-13 15:38:50 +0100

    improved-script

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){
 
-    $('li.accounts').hover(function(){
-        $('div.accounts-dropdown').toggleClass('active');
+    $('li.relative').hover(function(){
+        $(this).children('.dropdown').toggleClass('active');
     });
-
-    $('li.products').hover(function(){
-        $('div.products-dropdown').toggleClass('active');
-    });
-
-    $('li.company').hover(function(){
-        $('div.company-dropdown').toggleClass('active');
-    });
-
-    $('li.help').hover(function(){
-        $('div.help-dropdown').toggleClass('active');
-    });
-
+    
     $('li.language').hover(function(){
         $('div.language-dropdown').toggleClass('active');
         $('i.fa-chevron-down').toggleClass('active');
